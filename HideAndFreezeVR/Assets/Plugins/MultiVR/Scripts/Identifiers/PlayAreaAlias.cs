ce9168741fc3c0fcb7462e343a887b3924b9023a
Olaf <o.olaf@hotmail.com>
2018-09-18 11:34:21 +0200

    Setup Project
    Added the project to the github

---
+++ HideAndFreezeVR/Assets/Plugins/MultiVR/Scripts/Identifiers/PlayAreaAlias.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Identifier script for the play area alias.
+/// </summary>
+public class PlayAreaAlias : MultiVRAlias
+{
+    private PlayArea _playArea;
+
+    /// <summary>
+    /// The actual play area.
+    /// </summary>
+    public PlayArea playArea
+    {
+        get
+        {
+            if (_playArea == null)
+                _playArea = GetComponentInChildren<PlayArea>();
+
+            return _playArea;
+        }
+    }
+}
---
+++ HideAndFreezeVR/Assets/Scripts/HideController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideController : MonoBehaviour {
+
+    [SerializeField]
+    private HandAlias handAlias;
+    [SerializeField]
+    private MultiVRSetup multiVRSetup;
+    [SerializeField]
+    private GameObject steamVRController;
+
+    /// <summary>
+    /// Turns the controllers (in)visible based on the SDK used.
+    /// </summary>
+    /// <param name="value"></param>
+    public void HideVRController(bool value)
+    {
+        PlayAreaType playArea = multiVRSetup.playAreaAlias.playArea.type;
+
+        switch (playArea)
+        {
+            case PlayAreaType.SIMULATED:
+                break;
+            case PlayAreaType.STEAM:
+                foreach (Transform child in steamVRController.transform)
+                {
+                    Renderer renderer = child.GetComponent<MeshRenderer>();
+
+                    if (renderer != null)
+                    {
+                        renderer.enabled = value;
+                    }
+                }
+                break;
+            case PlayAreaType.OCULUS:
+                OvrAvatar avatar = multiVRSetup.playAreaAlias.GetComponentInChildren<OvrAvatar>(true);
+
+                avatar.ShowFirstPerson = value;
+                avatar.ShowControllers(value);
+                break;
+            case PlayAreaType.GEAR:
+                break;
+            default:
+                break;
+        }
+    }
+}
