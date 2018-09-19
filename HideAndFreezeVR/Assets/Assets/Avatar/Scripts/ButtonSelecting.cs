0cadc7421a20ba5f64997edb9f6c3e2029513f7c
Olaf <o.olaf@hotmail.com>
2018-09-19 11:15:15 +0200

    setup Avatar Set
    - Added the basic script that allows us to have an avatar
    - Made a Prefab for VR without Avatar

--- HideAndFreezeVR/Assets/Assets/Avatar/Scripts/ButtonSelecting.cs
+++ HideAndFreezeVR/Assets/Assets/Avatar/Scripts/ButtonSelecting.cs
@@ -6,79 +6,80 @@ using UnityEngine;
 [RequireComponent(typeof(VRTK.VRTK_ControllerEvents))]
 public class ButtonSelecting : MonoBehaviour
 {
-    private LineRenderer _lineRenderer;
+    private LineRenderer lineRenderer;
     [SerializeField]
-    private VRTK.VRTK_ControllerEvents _inputManager;
+    private VRTK.VRTK_ControllerEvents inputManager;
 
-    private bool _selectedUIItem;
-
-    [SerializeField]
-    private HandSide _side;
+    private bool selectedUIItem;
 
     [SerializeField]
-    private float _rayDistance = 200; //200 is recommended, because of the current distance between the vr player and the ui 
+    private HandSide side;
 
-    private bool _lineActive;
     [SerializeField]
-    private bool _delayActive;
+    private float rayDistance = 200; //200 is recommended, because of the current distance between the vr player and the ui 
+
+    private bool lineActive;
+    [SerializeField]
+    private bool delayActive;
 
     [SerializeField]
     [Tooltip("Position one for not selecting anything. Position two for when its selecting something")]
-    private Material[] _selectionColors;
+    private Material[] selectionColors;
 
     private void Start()
     {
-        _lineActive = false;
-        _selectedUIItem = false;
-        _delayActive = false;
+        lineActive = false;
+        selectedUIItem = false;
+        delayActive = false;
 
-        _lineRenderer = GetComponent<LineRenderer>();
-        _inputManager = GetComponent<VRTK.VRTK_ControllerEvents>();
+        lineRenderer = GetComponent<LineRenderer>();
+        inputManager = GetComponent<VRTK.VRTK_ControllerEvents>();
 
         HandAlias handAlias = GetComponentInChildren<HandAlias>();
         if (handAlias != null)
         {
-            _side = GetComponentInChildren<HandAlias>().side;
+            side = GetComponentInChildren<HandAlias>().side;
         }
 
         StartCoroutine(Delay(.1f));
     }
+
     private void Update()
     {
         //Disable/Enable's the line renderer depending on the state of the bool
-        if (_lineRenderer.enabled != _lineActive) _lineRenderer.enabled = _lineActive;
+        if (lineRenderer.enabled != lineActive) lineRenderer.enabled = lineActive;
 
-        switch (_lineActive)
+        switch (lineActive)
         {
             case true:
-                _lineRenderer.SetPosition(0, transform.position);
+                lineRenderer.SetPosition(0, transform.position);
 
                 RayCast();
                 break;
         }
 
         //Change the state of the bool
-        if (_inputManager.triggerClicked && !OVRManager.isHmdPresent && !_delayActive && !_selectedUIItem)
+        if (inputManager.triggerClicked && !OVRManager.isHmdPresent && !delayActive && !selectedUIItem)
         {
-            _lineActive = !_lineActive;
+            lineActive = !lineActive;
 
             StartCoroutine(Delay(.1f));
         }
-        else if(OVRManager.isHmdPresent && !_delayActive && !_selectedUIItem)
+        else if(OVRManager.isHmdPresent && !delayActive && !selectedUIItem)
         {
-            switch(_side)
+            switch(side)
             {
                 case HandSide.Left:
                     if(OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.LTouch))
                     {
-                        _lineActive = !_lineActive;
+                        lineActive = !lineActive;
                         StartCoroutine(Delay(.1f));
                     }
                     break;
                 case HandSide.Right:
                     if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.RTouch))
                     {
-                        _lineActive = !_lineActive;
+                        lineActive = !lineActive;
                         StartCoroutine(Delay(.1f));
                     }
                     break;
@@ -87,6 +88,11 @@ public class ButtonSelecting : MonoBehaviour
                     break;
             }
         }
+    }
+
+    public HandSide GethandSide()
+    {
+        return side;
     }
 
     private void RayCast()
@@ -99,7 +105,7 @@ public class ButtonSelecting : MonoBehaviour
         if(Physics.Raycast(rayCast, out rayCastHit, 200) && rayCastHit.collider.GetComponent<UnityEngine.UI.Button>() != null)
         {
             //Change the state of the bool
-            if (_inputManager.triggerClicked && !OVRManager.isHmdPresent && !_delayActive)
+            if (inputManager.triggerClicked && !OVRManager.isHmdPresent && !delayActive)
             {
                 UnityEngine.UI.Button button = rayCastHit.collider.GetComponent<UnityEngine.UI.Button>();
 
@@ -108,9 +114,9 @@ public class ButtonSelecting : MonoBehaviour
 
                 StartCoroutine(Delay(.1f));
             }
-            else if (OVRManager.isHmdPresent && !_delayActive)
+            else if (OVRManager.isHmdPresent && !delayActive)
             {
-                switch (_side)
+                switch (side)
                 {
                     case HandSide.Left:
                         if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.LTouch))
@@ -138,26 +144,26 @@ public class ButtonSelecting : MonoBehaviour
                 }
             }
 
-                _selectedUIItem = true;
+                selectedUIItem = true;
 
             //Set the line color to green and snap the position of the line
-            _lineRenderer.material = _selectionColors[1];
-            _lineRenderer.SetPosition(1, rayCastHit.collider.transform.position);
+            lineRenderer.material = selectionColors[1];
+            lineRenderer.SetPosition(1, rayCastHit.collider.transform.position);
         }
         else
         {
-            _selectedUIItem = false;
+            selectedUIItem = false;
 
             //Set the line color to red and allow the line to follow the ray
-            _lineRenderer.material = _selectionColors[0];
-            _lineRenderer.SetPosition(1, transform.position + (transform.forward * 200));
+            lineRenderer.material = selectionColors[0];
+            lineRenderer.SetPosition(1, transform.position + (transform.forward * 200));
         }
     }
 
     private IEnumerator Delay(float delaySeconds)
     {
         float delay = delaySeconds * 2;
-        _delayActive = true;
+        delayActive = true;
 
         while (delay > 0)
         {
@@ -165,6 +171,6 @@ public class ButtonSelecting : MonoBehaviour
             yield return new WaitForSeconds(delaySeconds);
         }
 
-        _delayActive = false;
+        delayActive = false;
     }
 }
---
+++ HideAndFreezeVR/Assets/Assets/Avatar/Scripts/VRAvatarController.cs
@@ -0,0 +1,198 @@
+﻿using RootMotion.FinalIK;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class VRAvatarController : MonoBehaviour
+{
+    [SerializeField]
+    private VRIK _avatarPrefab;
+    [SerializeField]
+    private GameObject _VRControllerPrefab;
+    [SerializeField]
+    private GameObject _containerObject;
+    [SerializeField]
+    private GameObject _VRRigPrefab;
+
+    public VRIK _actualAvatar;
+    private Collider[] _ownColliders;
+    private GameObject _VRRigObject;
+    private MultiVRSetup _multiVR;
+    private VRTK.VRTK_SDKManager _sdkManager;
+    private VRTK.VRTK_BezierPointerRenderer _rightControllerTeleport;
+    private VRTK.VRTK_BezierPointerRenderer _leftControllerTeleport;
+    private Vector3 _lastPosition;
+    private Quaternion _lastRotation;
+    private bool _haveIStarted = false;
+
+
+
+    private void Initialize()
+    {
+        // We should let the server know we this avatar controller exist on this client side so we can receive the root transform.
+        // This also lets the server know that we know this player exists, so that we can receive updates for it in the future.
+        Debug.Log(transform.position + " . " + transform.rotation);
+
+        _haveIStarted = true;
+    }
+
+    private void OnEnable()
+    {
+        VRTK.VRTK_DashTeleport.hasFinishedMoving += test;
+    }
+
+    private void test()
+    {
+        IKSolver solver = _avatarPrefab.GetIKSolver();
+        IKSolverVR solverVR = solver as IKSolverVR;
+        solverVR.Reset();
+    }
+
+    /// <summary>
+    /// We received a server update with a position and rotation.
+    /// </summary>
+    /// <param name="position"></param>
+    /// <param name="rotation"></param>
+    public void ProcessServerUpdate(Vector3 position, Quaternion rotation)
+    {
+        if (!_haveIStarted) return;
+
+        // If we already exist, we need to update our values.
+        if (_containerObject != null)
+        {
+            _updateRootTransform(position, rotation);
+            return;
+        }
+
+        // Otherwise we initialize our play area.
+        VRSetup(position, rotation);
+    }
+
+    /// <summary>
+    /// Update the positions of our container and VR rig (if applicable)
+    /// </summary>
+    /// <param name="position"></param>
+    /// <param name="rotation"></param>
+    protected void _updateRootTransform(Vector3 position, Quaternion rotation)
+    {
+        Transform container = _containerObject.transform;
+
+        container.position = position;
+        container.rotation = rotation;
+
+        // If we are the local player, we must sync the container to our play area.
+        Transform playArea = _multiVR.playAreaAlias.transform;
+
+        playArea.position = container.position;
+        playArea.rotation = container.rotation;
+
+        _capturePlayAreaTransform();
+    }
+
+    /// <summary>
+    /// Initialize the VR rig for this avatar.
+    /// </summary>
+    /// <param name="position"></param>
+    /// <param name="rotation"></param>
+    private void VRSetup(Vector3 position, Quaternion rotation)
+    {
+        _containerObject = new GameObject("VRContainer");
+        _containerObject.transform.position = position;
+        _containerObject.transform.rotation = rotation;
+
+        transform.SetParent(_containerObject.transform, true);
+        _actualAvatar = Instantiate(_avatarPrefab, Vector3.zero, Quaternion.identity);
+        _actualAvatar.solver.spine.headTarget = transform;
+        _actualAvatar.transform.SetParent(_containerObject.transform, false);
+        _actualAvatar.transform.rotation = _actualAvatar.transform.rotation * Quaternion.Inverse(rotation);
+        //Current client owns this player
+        //create camera rig and attach player model to it
+
+        _VRRigObject = Instantiate(_VRRigPrefab, transform.position, transform.rotation);
+        _VRRigObject.transform.SetParent(_containerObject.transform, false);
+        _VRRigObject.transform.localPosition = Vector3.zero;
+
+        _sdkManager = _VRRigObject.GetComponentInChildren<VRTK.VRTK_SDKManager>();
+        _multiVR = _VRRigObject.GetComponentInChildren<MultiVRSetup>();
+
+        GameObject leftController = Instantiate(_VRControllerPrefab, _containerObject.transform);
+        GameObject rightController = Instantiate(_VRControllerPrefab, _containerObject.transform);
+        leftController.name = "LeftController (Clone)";
+        rightController.name = "RightController (Clone)";
+
+        // Attach left hand
+        MultiVRUtil.MakeReferral(_multiVR.leftHandAlias.gameObject);
+        _multiVR.leftHandAlias.transform.SetParent(leftController.transform);
+
+        // Attach right hand
+        MultiVRUtil.MakeReferral(_multiVR.rightHandAlias.gameObject);
+        _multiVR.rightHandAlias.transform.SetParent(rightController.transform);
+
+        _sdkManager.scriptAliasLeftController = leftController;
+        _sdkManager.scriptAliasRightController = rightController;
+
+        //// Create a dummy that inverts the rotation from the spawn position
+        //GameObject leftInversion = new GameObject("Rotation Inversion (Left)");
+        //leftInversion.transform.localRotation = Quaternion.Inverse(rotation);
+
+        //leftInversion.transform.SetParent(_multiVR.leftHandAlias.avatarOffset.parent, false);
+        //_multiVR.leftHandAlias.avatarOffset.SetParent(leftInversion.transform, false);
+
+        //// Create a dummy that inverts the rotation from the spawn position.
+        //GameObject rightInversion = new GameObject("Rotation Inversion (Right)");
+        //rightInversion.transform.localRotation = Quaternion.Inverse(rotation);
+
+        //rightInversion.transform.SetParent(_multiVR.rightHandAlias.avatarOffset.parent, false);
+        //_multiVR.rightHandAlias.avatarOffset.SetParent(rightInversion.transform, false);
+
+        // Set actual avatar transforms.
+        _actualAvatar.solver.spine.headTarget = _multiVR.headAlias.avatarOffset.transform;
+        _actualAvatar.solver.leftArm.target = _multiVR.leftHandAlias.avatarOffset.transform;
+        _actualAvatar.solver.rightArm.target = _multiVR.rightHandAlias.avatarOffset.transform;
+
+        //Get Teleport
+        _rightControllerTeleport = rightController.GetComponent<VRTK.VRTK_BezierPointerRenderer>();
+        _leftControllerTeleport = leftController.GetComponent<VRTK.VRTK_BezierPointerRenderer>();
+
+        this.transform.SetParent(_actualAvatar.solver.spine.headTarget, false);
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.identity;
+        transform.localScale = Vector3.one;
+
+        _ownColliders = transform.root.GetComponentsInChildren<Collider>();
+
+        _capturePlayAreaTransform();
+
+        Debug.Log("Avatar: " + _actualAvatar);
+    }
+
+    protected virtual void LateUpdate()
+    {
+        _updatePlayAreaTransform();
+    }
+
+    private void _capturePlayAreaTransform()
+    {
+        Transform target = _multiVR.playAreaAlias.transform;
+        _lastPosition = target.position;
+        _lastRotation = target.rotation;
+    }
+
+    private void _updatePlayAreaTransform()
+    {
+        if (_multiVR == null)
+            return;
+
+        Transform target = _multiVR.playAreaAlias.transform;
+
+        if (!_lastPosition.Equals(target.position) || !_lastRotation.Equals(target.rotation))
+        {
+            // Capture new values.
+            _capturePlayAreaTransform();
+
+            ProcessServerUpdate(target.position, target.rotation);
+        }
+    }
+}
