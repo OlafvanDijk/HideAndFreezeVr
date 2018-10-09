00637fbdc683e874795a0c0e01ef120dfc55e576
Olaf <o.olaf@hotmail.com>
2018-10-09 15:05:01 +0200

    Out of bounds for the dolls

---
+++ HideAndFreezeVR/Assets/Assets/Avatar/Scripts/OutOfBounds.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutOfBounds : MonoBehaviour {
+
+    [SerializeField]
+    private string tag;
+
+    private Vector3 originalPosition;
+    private Quaternion originalRotation;
+
+    // Use this for initialization
+    void Start () {
+        originalPosition = transform.position;
+        originalRotation = transform.rotation;
+
+	}
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag.Equals(tag))
+        {
+            Rigidbody rigidbody = GetComponent<Rigidbody>();
+            if (rigidbody != null)
+            {
+                rigidbody.velocity = Vector3.zero;
+                rigidbody.angularVelocity = Vector3.zero;
+            }
+            this.transform.position = originalPosition;
+            this.transform.rotation = originalRotation;
+        }
+    }
+}
