6ee0269aaf1230735c82e794b37fe5d7afc6bfaa
Niels Verheijen <kahlan147@gmail.com>
2018-10-12 14:47:34 +0200

    Seperate spawn room

---
+++ HideAndFreezeVR/Assets/Assets/Networking/Scripts/VR/Teleporter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour {
+
+    public Transform TeleportTo;
+    public ParticleSystem particleSystem;
+
+    private void OnTriggerEnter(Collider other)
+    {
+       LocationDataPlayer player = other.gameObject.GetComponentInParent<LocationDataPlayer>();
+        if(player != null)
+        {
+            StartCoroutine(Teleport(player));
+        }
+    }
+
+    private IEnumerator Teleport(LocationDataPlayer player)
+    {
+        yield return new WaitForSeconds(1);
+        particleSystem.Play();
+
+        yield return new WaitForSeconds(5);
+
+        player.gameObject.transform.parent.position = Vector3.zero;
+
+        particleSystem.Stop();
+
+    }
+}
